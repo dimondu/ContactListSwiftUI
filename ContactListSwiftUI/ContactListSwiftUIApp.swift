bc7acfbaa7d6c8b7b44f9a3bbbbf1d79cb8d4b10
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-09-23 15:38:18 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Дмитрий Дуров on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
